6e753adc91ab8977ddf65420d890e20b3b81d899
Voidism <80137966+voidisms@users.noreply.github.com>
2021-04-06 20:14:39 -0400

    Add files via upload

---
+++ whatsappspam.py
@@ -0,0 +1,18 @@
+from pynput.keyboard import Key, Controller
+import time
+
+
+message = "nova is awesome!"
+keyboard = Controller()
+
+time.sleep(5)
+
+
+for num in range(100):
+    for letter in message:
+        keyboard.press(letter)
+        keyboard.release(letter)
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
+    time.sleep(0.1)
+    
